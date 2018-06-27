cee1353cb16f31137dcf32bc3cbc1e754ebc798d
Max Base <MaxBaseCode@Gmail.Com>
2018-06-27 14:37:32 +0430

    Update NumberInput.js

--- NumberInput.js
+++ NumberInput.js
@@ -1,1 +1,10 @@
-
+/**
+*
+* @Name : NumberInput.js
+* @Version : 1.0
+* @Programmer : Max
+* @Date : 2018-06-27
+* @Released under : https://github.com/BaseMax/NumberInputJs/blob/master/LICENSE
+* @Repository : https://github.com/BaseMax/NumberInputJs
+*
+**/
